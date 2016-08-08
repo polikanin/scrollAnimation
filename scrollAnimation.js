8269f48e5ee23875f8d94c33e7ca4997f4e19258
Anatoliy <papik5@mail.ru>
2016-08-08 16:14:37 +0300

    version 1.1.0

--- scrollAnimation.js
+++ scrollAnimation.js
@@ -2,47 +2,97 @@ var scrollAnimation = function (params) {
     var options = {
         element: params.element,                                // animated DOM element
         direction: params.direction,                            // hide block or not if scrolling to top
-        appearanceField: params.appearanceField || 4,           // part of screen for appearance animated blocks
-        identicalAnimation: params.identicalAnimation,          // is the animation the same or not
-        animation: params.animation || 'fade-out'               // type of animation
+        appearanceField: params.appearanceField || 4,           // part of screen for appearance animated elements
+        animation: params.animation || 'fade-out',              // type of animation
+        animateCss: params.animateCss                           // if need to use AnimateCss
     };
 
     var showClass = 'show';
-    var animatedClass = 'scroll__animation__' + options.animation;
-    var blocks = document.querySelectorAll(options.element);
+    var animatedClass = 'scroll__animation__' + options.animation;  // class of animated elements
+    var elements = options.animateCss ? document.querySelectorAll('.animated') : document.querySelectorAll(options.element);
     var vh = window.innerHeight;
+    var position = [];  // array for collecting elements position
 
-    window.onresize = function () {
-        vh = window.innerHeight;
+    var getElementsPosition = function(counter){
+        return elements[counter].getBoundingClientRect().top + window.pageYOffset;   //get elements position from top
     };
 
-    if(options.identicalAnimation == undefined){
-        for(var i = 0; i < blocks.length; i++){
-            blocks[i].classList.add(animatedClass);
-        }
-    }
-
-    if(options.direction && options.direction){
-        window.onscroll = function () {
-            for(var i = 0; i < blocks.length; i++){
-                var self = blocks[i];
-                if(self.getBoundingClientRect().top - vh < - vh/options.appearanceField){
+    var forward = function(){
+        var start = window.pageYOffset + vh - (vh/options.appearanceField);
+        for(var i = 0; i < elements.length; i++){
+            var self = elements[i];
+            if(position[i] < start){
+                if(options.animateCss){
+                    self.classList.add(newClass[i]);
+                }
+                else{
                     self.classList.add(showClass);
                 }
             }
-        };
-    }
-    else{
-        window.onscroll = function () {
-            for(var i = 0; i < blocks.length; i++){
-                var self = blocks[i];
-                if(self.getBoundingClientRect().top - vh < - vh/options.appearanceField){
+        }
+    };
+
+    var reverse = function(){
+        var start = window.pageYOffset + vh - (vh/options.appearanceField);
+        for(var i = 0; i < elements.length; i++){
+            var self = elements[i];
+            if(position[i] < start){
+                if(options.animateCss){
+                    self.classList.add(newClass[i]);
+                }
+                else{
                     self.classList.add(showClass);
+                }
+            }
+            else{
+                if(options.animateCss){
+                    self.classList.remove(newClass[i]);
                 }
                 else{
                     self.classList.remove(showClass);
                 }
             }
-        };
+        }
+    };
+
+    for(var i = 0; i < elements.length; i++){
+        position[i] = getElementsPosition(i);
+    }
+
+    if(options.animateCss){
+        var reg = /scroll-[a-zA-z]*/;
+        var newClass = [];
+        for(var i = 0; i < elements.length; i++){
+            var classNameList = elements[i].className;
+            var className = classNameList.match(reg);
+            newClass[i] = className[0].substring(7);
+            elements[i].classList.remove(className);
+        }
+    }
+    else{
+        for(var i = 0; i < elements.length; i++){
+            elements[i].classList.add(animatedClass);
+        }
+    }
+
+    window.onresize = function () {
+        vh = window.innerHeight;
+        for(var i = 0; i < elements.length; i++){
+            if(options.animateCss){
+                position[i] = getElementsPosition(i);
+            }
+            else{
+                elements[i].classList.remove(animatedClass);
+                position[i] = getElementsPosition(i);
+                elements[i].classList.add(animatedClass);
+            }
+        }
+    };
+
+    if(options.direction){
+        window.onscroll = forward;
+    }
+    else{
+        window.onscroll = reverse
     }
 };
